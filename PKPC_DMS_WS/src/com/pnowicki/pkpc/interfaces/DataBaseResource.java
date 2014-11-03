8865044827a223dc9a4ad9611bc826b137abbac6
Paweł Nowicki <mrhydenowy@gmail.com>
2014-11-03 22:49:43 +0100

    Dodanie interfejsu oraz klasy implementujacej

---
+++ PKPC_DMS_WS/src/com/pnowicki/pkpc/interfaces/DataBaseResource.java
@@ -0,0 +1,16 @@
+package com.pnowicki.pkpc.interfaces;
+
+import java.io.File;
+
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+
+public interface DataBaseResource {
+	public Response addDocument(String fileName, String fileSignature, String filePath, File file, String endFile) throws Exception;
+	public String getUserRole(String userName,	String userPassword) throws Exception;
+	public JSONArray getFiles();
+	public JSONArray getBooleans();
+	public JSONArray getInts();
+	public Response setConsidered(String fileIndex);
+}
